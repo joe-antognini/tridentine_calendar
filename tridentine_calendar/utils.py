667ec838ece7da674b348beeb9964b0612c89ce0
Joe Antognini <joe@whisper.ai>
2019-07-01 11:02:37 -0700

    Migrate from feast_dates to movable_feasts.

--- tridentine_calendar/utils.py
+++ tridentine_calendar/utils.py
@@ -38,6 +38,35 @@ ORDINALS = {
 
 
 @functools.lru_cache()
+def computus(year):
+    """Calculate the date of Easter.
+
+    Args:
+        year: Integer with the year.
+
+    Returns:
+        A `datetime.Date` object with the date of Easter.
+
+    """
+    a = year % 19
+    b = year // 100
+    c = year % 100
+    d = b // 4
+    e = b % 4
+    f = (b + 8) // 25
+    g = (b - f + 1) // 3
+    h = (19 * a + b - d - g + 15) % 30
+    i = c // 4
+    k = c % 4
+    l = (32 + 2 * e + 2 * i - h - k) % 7
+    m = (a + 11 * h + 22 * l) // 451
+    month = (h + l - 7 * m + 114) // 31
+    day = ((h + l - 7 * m + 114) % 31) + 1
+
+    return dt.date(year, month, day)
+
+
+@functools.lru_cache()
 def liturgical_year_start(year):
     """Calculate the start of the liturgical year.
 
